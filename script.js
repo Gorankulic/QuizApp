86efcf3ceb1e23d9d932edf81437b68bd4573142
Goran <gorankulic@outlook.comm>
2023-04-07 22:41:38 +0200

    added json array to java script

---
+++ script.js
@@ -0,0 +1,41 @@
+let questions = [{
+        question: "What is the capital of France?",
+        answer_1: "London",
+        answer_2: "Berlin",
+        answer_3: "Paris",
+        answer_4: "Madrid",
+        right_answer: "Paris"
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        answer_1: "Pablo Picasso",
+        answer_2: "Claude Monet",
+        answer_3: "Leonardo da Vinci",
+        answer_4: "Vincent van Gogh",
+        right_answer: "Leonardo da Vinci"
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        answer_1: "Mount Everest",
+        answer_2: "Mount Kilimanjaro",
+        answer_3: "Mount Fuji",
+        answer_4: "Mount Whitney",
+        right_answer: "Mount Everest"
+    },
+    {
+        question: "What is the largest mammal in the world?",
+        answer_1: "Elephant",
+        answer_2: "Blue whale",
+        answer_3: "Giraffe",
+        answer_4: "Hippopotamus",
+        right_answer: "Blue whale"
+    },
+    {
+        question: "Who wrote the novel 'Pride and Prejudice'?",
+        answer_1: "Emily Bronte",
+        answer_2: "Charlotte Bronte",
+        answer_3: "Jane Austen",
+        answer_4: "Virginia Woolf",
+        right_answer: "Jane Austen"
+    }
+];
