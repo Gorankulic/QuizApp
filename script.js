d24a72e481880be0e7d9b2355b435c2075af23bd
Goran <gorankulic@outlook.comm>
2023-04-07 23:02:07 +0200

    added 20 questions to json array

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let questions = [{
         answer_2: "Berlin",
         answer_3: "Paris",
         answer_4: "Madrid",
-        right_answer: "Paris"
+        right_answer: 3
     },
     {
         question: "Who painted the Mona Lisa?",
@@ -12,7 +12,7 @@ let questions = [{
         answer_2: "Claude Monet",
         answer_3: "Leonardo da Vinci",
         answer_4: "Vincent van Gogh",
-        right_answer: "Leonardo da Vinci"
+        right_answer: 3
     },
     {
         question: "What is the tallest mountain in the world?",
@@ -20,7 +20,7 @@ let questions = [{
         answer_2: "Mount Kilimanjaro",
         answer_3: "Mount Fuji",
         answer_4: "Mount Whitney",
-        right_answer: "Mount Everest"
+        right_answer: 1
     },
     {
         question: "What is the largest mammal in the world?",
@@ -28,7 +28,7 @@ let questions = [{
         answer_2: "Blue whale",
         answer_3: "Giraffe",
         answer_4: "Hippopotamus",
-        right_answer: "Blue whale"
+        right_answer: 2
     },
     {
         question: "Who wrote the novel 'Pride and Prejudice'?",
@@ -36,6 +36,270 @@ let questions = [{
         answer_2: "Charlotte Bronte",
         answer_3: "Jane Austen",
         answer_4: "Virginia Woolf",
-        right_answer: "Jane Austen"
+        right_answer: 3
+    },
+    {
+        question: "What is the smallest planet in our solar system?",
+        answer_1: "Mars",
+        answer_2: "Mercury",
+        answer_3: "Jupiter",
+        answer_4: "Venus",
+        right_answer: 2
+    },
+    {
+        question: "Who was the first person to step on the Moon?",
+        answer_1: "Neil Armstrong",
+        answer_2: "Buzz Aldrin",
+        answer_3: "Michael Collins",
+        answer_4: "Yuri Gagarin",
+        right_answer: 1
+    },
+    {
+        question: "What is the largest organ in the human body?",
+        answer_1: "Lungs",
+        answer_2: "Heart",
+        answer_3: "Liver",
+        answer_4: "Skin",
+        right_answer: 4
+    },
+    {
+        question: "What is the name of the Greek god of the sea?",
+        answer_1: "Zeus",
+        answer_2: "Poseidon",
+        answer_3: "Hades",
+        answer_4: "Apollo",
+        right_answer: 2
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answer_1: "Au",
+        answer_2: "Ag",
+        answer_3: "Fe",
+        answer_4: "Cu",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's largest desert?",
+        answer_1: "Sahara",
+        answer_2: "Arctic",
+        answer_3: "Gobi",
+        answer_4: "Kalahari",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the first artificial satellite launched into space?",
+        answer_1: "Sputnik 1",
+        answer_2: "Explorer 1",
+        answer_3: "Vanguard 1",
+        answer_4: "Luna 1",
+        right_answer: 1
+    },
+    {
+        question: "What is the largest continent in the world?",
+        answer_1: "North America",
+        answer_2: "South America",
+        answer_3: "Asia",
+        answer_4: "Africa",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the world's largest ocean?",
+        answer_1: "Indian",
+        answer_2: "Atlantic",
+        answer_3: "Arctic",
+        answer_4: "Pacific",
+        right_answer: 4
+    },
+    {
+        question: "Who is the author of the Harry Potter series of books?",
+        answer_1: "J.K. Rowling",
+        answer_2: "Stephenie Meyer",
+        answer_3: "Suzanne Collins",
+        answer_4: "Veronica Roth",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's longest river?",
+        answer_1: "Nile",
+        answer_2: "Amazon",
+        answer_3: "Yangtze",
+        answer_4: "Mississippi",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's largest waterfall?",
+        answer_1: "Angel",
+        answer_2: "Niagara",
+        answer_3: "Victoria",
+        answer_4: "Iguazu",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the device used to measure earthquakes?",
+        answer_1: "Barometer",
+        answer_2: "Anemometer",
+        answer_3: "Seismometer",
+        answer_4: "Hygrometer",
+        right_answer: 3
+    },
+    {
+        question: "What is the capital of Australia?",
+        answer_1: "Sydney",
+        answer_2: "Melbourne",
+        answer_3: "Canberra",
+        answer_4: "Brisbane",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the person who founded Microsoft?",
+        answer_1: "Steve Jobs",
+        answer_2: "Bill Gates",
+        answer_3: "Mark Zuckerberg",
+        answer_4: "Larry Page",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the highest mountain in Africa?",
+        answer_1: "Mount Everest",
+        answer_2: "Mount Kilimanjaro",
+        answer_3: "Mount Aconcagua",
+        answer_4: "Mount Vinson",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the world's largest lizard?",
+        answer_1: "Iguana",
+        answer_2: "Gila Monster",
+        answer_3: "Komodo Dragon",
+        answer_4: "Chameleon",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the world's largest bird?",
+        answer_1: "Ostrich",
+        answer_2: "Eagle",
+        answer_3: "Emu",
+        answer_4: "Albatross",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's largest carnivorous marsupial?",
+        answer_1: "Kangaroo",
+        answer_2: "Koala",
+        answer_3: "Tasmanian Devil",
+        answer_4: "Wallaby",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the first man to walk on the bottom of the ocean?",
+        answer_1: "Jacques Cousteau",
+        answer_2: "Robert Ballard",
+        answer_3: "James Cameron",
+        answer_4: "Edmund Hillary",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the largest bone in the human body?",
+        answer_1: "Femur",
+        answer_2: "Humerus",
+        answer_3: "Radius",
+        answer_4: "Ulna",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's largest coral reef?",
+        answer_1: "Great Barrier Reef",
+        answer_2: "Belize Barrier Reef",
+        answer_3: "Red Sea Coral Reef",
+        answer_4: "Andaman Coral Reef",
+        right_answer: 1
+    },
+    {
+        question: "What is the name of the world's largest saltwater lake?",
+        answer_1: "Great Salt Lake",
+        answer_2: "Caspian Sea",
+        answer_3: "Dead Sea",
+        answer_4: "Lake Superior",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the world's largest hot desert?",
+        answer_1: "Gobi Desert",
+        answer_2: "Kalahari Desert",
+        answer_3: "Sahara Desert",
+        answer_4: "Mojave Desert",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the largest planet in our solar system?",
+        answer_1: "Venus",
+        answer_2: "Jupiter",
+        answer_3: "Mars",
+        answer_4: "Neptune",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the world's largest land mammal?",
+        answer_1: "Hippopotamus",
+        answer_2: "Giraffe",
+        answer_3: "Elephant",
+        answer_4: "Rhino",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the instrument used to measure atmospheric pressure?",
+        answer_1: "Hygrometer",
+        answer_2: "Barometer",
+        answer_3: "Anemometer",
+        answer_4: "Thermometer",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the smallest planet in our solar system?",
+        answer_1: "Venus",
+        answer_2: "Jupiter",
+        answer_3: "Mercury",
+        answer_4: "Mars",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the world's largest reptile?",
+        answer_1: "Python",
+        answer_2: "Alligator",
+        answer_3: "Crocodile",
+        answer_4: "Turtle",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the instrument used to measure wind speed?",
+        answer_1: "Hygrometer",
+        answer_2: "Barometer",
+        answer_3: "Anemometer",
+        answer_4: "Thermometer",
+        right_answer: 3
+    },
+    {
+        question: "What is the name of the smallest country in the world?",
+        answer_1: "Monaco",
+        answer_2: "Vatican City",
+        answer_3: "Liechtenstein",
+        answer_4: "San Marino",
+        right_answer: 2
+    },
+    {
+        question: "What is the name of the world's largest estuary?",
+        answer_1: "Chesapeake Bay",
+        answer_2: "Bay of Fundy",
+        answer_3: "San Francisco Bay",
+        answer_4: "Rio de la Plata",
+        right_answer: 4
+    },
+    {
+        question: "What is the name of the world's largest continent?",
+        answer_1: "North America",
+        answer_2: "South America",
+        answer_3: "Europe",
+        answer_4: "Asia",
+        right_answer: 4
     }
 ];
